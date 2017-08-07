d2435f395b5cf37e5d68e4d365d8ebbbdafb8eea
goldeneyezhang <goldeneyezhang@163.com>
2017-08-07 18:03:43 +0800

    more task

--- ConsoleTask/Program.cs
+++ ConsoleTask/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleTask
@@ -9,28 +10,54 @@ namespace ConsoleTask
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			var task4 = new Task(() =>
-			{
-				Console.WriteLine("Task 4 Begin");
-				System.Threading.Thread.Sleep(2000);
-				Console.WriteLine("Task 4 Finish");
-			});
-			var task5 = new Task(() =>
-			{
-				Console.WriteLine("Task 5 Begin");
-				System.Threading.Thread.Sleep(3000);
-				Console.WriteLine("Task 5 Finish");
-			});
-
-			task4.Start();
-			task5.Start();
-			var result = task4.ContinueWith<string>(task =>
+		{ 
+			var tokenSource = new CancellationTokenSource();
+			var token = tokenSource.Token;
+			var task = Task.Factory.StartNew(() =>
 			  {
-				  Console.WriteLine("task1 finished");
-				  return "This is task result!";
-			  });
-			Console.WriteLine(result.Result.ToString());
+				  for (var i = 0; i < 1000; i++)
+				  {
+					  System.Threading.Thread.Sleep(1000);
+					  if(tokenSource.IsCancellationRequested)
+					  {
+						  Console.WriteLine("Abort mission success");
+						  return;
+					  }
+				  }
+			  },token);
+			token.Register(() =>
+			{
+				Console.WriteLine("Cancelled");
+			}
+			);
+			Console.WriteLine("Press enter to cancel task");
+			Console.ReadKey();
+			tokenSource.Cancel();
+			//SpinLock spinLock = new SpinLock();
+			//spinLock.
+			Console.Read();
+			//var task4 = new Task(() =>
+			//{
+			//	Console.WriteLine("Task 4 Begin");
+			//	System.Threading.Thread.Sleep(2000);
+			//	Console.WriteLine("Task 4 Finish");
+			//});
+			//var task5 = new Task(() =>
+			//{
+			//	Console.WriteLine("Task 5 Begin");
+			//	System.Threading.Thread.Sleep(3000);
+			//	Console.WriteLine("Task 5 Finish");
+			//});
+
+			//task4.Start();
+			//task5.Start();
+			//var result = task4.ContinueWith<string>(task =>
+			//  {
+			//	  Console.WriteLine("task1 finished");
+			//	  return "This is task result!";
+			//  });
+			//Console.WriteLine(result.Result.ToString());
+			
 			//Task.WaitAll(task4, task5);
 			//Console.WriteLine("All task finished!");
 			//var task1 = new Task(() =>
